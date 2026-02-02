fb43968a00d6247e31b2bddc862826a0f3041760
vansh216 <216386028+vansh216@users.noreply.github.com>
2026-02-03 00:35:57 +0530

    Add Navbar Section

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Menu from '../components/menu'
-import Footer from '../components/Footer'
-import Header from '../components/header'
 import Hero from '../components/Hero'
+
 
 
 
 const Landing = () => {
   return (
     <div>
+      <Navbar/>
      
       <Hero/>
      
